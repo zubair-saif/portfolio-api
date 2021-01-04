01f4f7fb66b10d04e5b5e421d5dc0699ba069335
zubair-saif <zubairsaif700@gmail.com>
2021-01-05 00:01:58 +0500

    ..

--- app/media/controllers/mediaController.js
+++ app/media/controllers/mediaController.js
@@ -1,13 +1,2 @@
 
 const { Media } = require('../models/mediaM');
-
-module.exports.uploadProgramsIcon = async (req, res) => {
-
-    const programIcon = await Media.create({
-        destination: [req.file.path],
-        type: req.body.type
-    })
-    const result = await programIcon.save();
-    res.send(result);
-
-}
--- app/media/routes/mediaRoute.js
+++ app/media/routes/mediaRoute.js
@@ -1,15 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mediaController = require('../controllers/mediaController')
-const multer = require('multer');
 
-// let storage = multer.diskStorage({
-//     destination: 'public/program/',
-//     filename: (req, file, cb) => {
-//         cb(null, Date.now() + '-' + file.originalname);
-//     }
-// })
-// let upload = multer({ storage: storage }).single('programIcon')
 
 router.post('/programs',
     // upload,
