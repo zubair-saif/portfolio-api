8794f89770c819c719dd4d7ee3096de3fe28f7d8
zubair-saif <zubairsaif700@gmail.com>
2021-01-04 23:53:10 +0500

    port change

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ connectDB();
 
 app.use(error);
 
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT || 4400;
 app.listen(PORT, () => {
     console.log(
         `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
