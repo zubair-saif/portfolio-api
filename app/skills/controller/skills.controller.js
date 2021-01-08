8ce554cd0420c00c2a81658703179e8d24fad9ea
zubair-saif <zubairsaif700@gmail.com>
2021-01-08 12:17:51 +0500

    update skills

--- app/skills/controller/skills.controller.js
+++ app/skills/controller/skills.controller.js
@@ -3,7 +3,7 @@ const { Skills } = require('../models/skills.models');
 
 module.exports.create = async (req, res) => {
 
-    console.log(req.body.title, req.body.description)
+    console.log(req.route, "sssss")
 
 
     const skill = await Skills.create({
@@ -12,6 +12,25 @@ module.exports.create = async (req, res) => {
     });
     skill.save();
     res.json({ message: "Successfully Created" });
+
+
+}
+
+module.exports.update = async (req, res) => {
+
+
+    const skill = await Skills.findByIdAndUpdate(req.params._id,
+
+        {
+            $set: {
+                title: req.body.title,
+                description: req.body.description
+            }
+        },
+        { new: true }
+    );
+    res.save(skill);
+    return res.json({ message: "success" });
 
 
 }
